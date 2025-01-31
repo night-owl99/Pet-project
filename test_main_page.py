0085b98a48054b8f566529cf51f2c67b5d701938
kksen <k.ksenia99@gmail.com>
2025-01-31 08:36:37 +0300

    base tests for login page added

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,5 @@
 from pages.main_page import MainPage
-from pages.base_page import BasePage
-from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     
@@ -8,3 +7,24 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) 
     page.open()
     page.go_to_login_page()
+    
+def test_url_is_for_login(browser):
+    
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link) 
+    page.open()
+    page.should_be_login_url()
+    
+def test_login_form_is_present(browser):
+    
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link) 
+    page.open()
+    page.should_be_login_form()
+    
+def test_register_form_is_present(browser):
+    
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link) 
+    page.open()
+    page.should_be_register_form()
