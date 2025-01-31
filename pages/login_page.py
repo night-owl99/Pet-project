47b391b9a717ee64d0c7a0a82a6bfe40f0273e3b
kksen <k.ksenia99@gmail.com>
2025-01-31 08:47:58 +0300

    class deleted from login_page file

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,10 +2,6 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
 
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "The url is not for the login page"
--- test_main_page.py
+++ test_main_page.py
@@ -7,10 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) 
     page.open()
     page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
     
-def test_url_is_for_login(browser):
+def test_should_be_login_page(browser):
     
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link) 
